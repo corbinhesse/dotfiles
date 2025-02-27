817a70ba228d053080f5be900559e00227961794
Corbin Hesse <hesse.corbin@gmail.com>
2025-02-27 07:50:22 -0600

    feat: ghostty local config

--- ghostty/install.sh
+++ ghostty/install.sh
@@ -3,4 +3,5 @@
 info "ghostty"
 
 symlink "config" "config" "config/ghostty"
+symlink "config.local" "config.local" "config/ghostty"
 symlink "MaterialCorbin" "MaterialCorbin" "config/ghostty/themes"
